4365d093e3afc065dbcdcb0aa38a85bb5493129e
MANISH MACHHA <manishmachha@gmail.com>
2025-03-10 14:02:04 +0530

    added notfound exception globally

--- src/main/java/com/prep/restapis/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/prep/restapis/exceptionHandlers/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.prep.restapis.exceptionHandlers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +19,14 @@ public class GlobalExceptionHandler {
         ex.getBindingResult().getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
